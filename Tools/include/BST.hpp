86b0892a506b13a63cef41450eaf67f17da12054
Dragan Dragos Nicolae <dra.dragos.dd@gmail.com>
2026-05-08 21:46:17 +0300

    wrote bst header file

--- Tools/include/BST.hpp
+++ Tools/include/BST.hpp
@@ -6,43 +6,70 @@ namespace tools {
 
     template <typename T>
     class BST {
-        public: 
+        private: 
             struct BSTNode {
                 BSTNode* left = nullptr;
                 BSTNode* right = nullptr;
-                T* value;
+                T* data;
 
-                BSTNode(T value) : value(new T(value)) {};
-                ~BSTNode() { delete value; }
+                BSTNode(T data) : data(new T(data)) {};
+                ~BSTNode() { delete data; }
             };
-
-            typedef tools::Optional<BSTNode*> OptionalNode;
 
         public:
             BST() {};
+            ~BST() {};
+            BST(const BST&) = delete; // disable copy constructor
+            BST& operator=(const BST&) = delete;
 
-            void insert(T value);
-            void remove(T value);
-            // OptionalNode find(T value);
-            // OptionalNode findInRange(T min, T max);
+            void insert(T value) { insert(value, root); }
+            void remove(T value) { remove(value, root); }
+
+            tools::Optional<T> find(T value) const { return find(value, root); }
+            tools::Optional<T> findInRange(T min, T max) const { return findInRange(min, max, root); }
+
+            template <typename Criterion>
+            tools::Optional<T> findLinear(Criterion criterion) const { return findLinear(criterion, root); } // returns the first node that makes the criterion return true
 
             template <typename Func>
-            void traverseInOrder(Func function, BSTNode* root = nullptr);
+            void traverseInOrder(Func function) const { traverseInOrder(function, root); }
             template <typename Func>
-            void traversePreOrder(Func function, BSTNode* root = nullptr);
+            void traversePreOrder(Func function) const { traversePreOrder(function, root); }
             template <typename Func>
-            void traversePostOrder(Func function, BSTNode* root = nullptr);
+            void traversePostOrder(Func function) const { traversePostOrder(function, root); }
 
-            T* getMin();
-            T* getMax();
-            T* getKthLargest(unsigned int k);
+            tools::Optional<T> getMin() const;
+            tools::Optional<T> getMax() const;
+            tools::Optional<T> getKthLargest(unsigned int k) const;
 
-            unsigned int getHeight();
-            unsigned int getNumberOfLeafs();
-            bool isBalanced();
+            unsigned int getHeight() const { return getHeight(root); }
+            unsigned int getNumberOfLeafs() const { return getNumberOfLeafs(root); }
+            bool isBalanced() const { return isBalanced(root); }
         
-        public:
+        private:
             BSTNode* root = nullptr;
+        
+        // private functions to help recursion
+        private:
+            void insert(T value, BSTNode*& node);
+            void remove(T value, BSTNode*& node);
+
+            tools::Optional<T> find(T value, BSTNode* node) const;
+            tools::Optional<T> findInRange(T min, T max, BSTNode* node) const;
+
+            template <typename Criterion>
+            tools::Optional<T> findLinear(Criterion criterion, BSTNode* node) const;
+
+            template <typename Func>
+            void traverseInOrder(Func function, BSTNode* node) const;
+            template <typename Func>
+            void traversePreOrder(Func function, BSTNode* node) const;
+            template <typename Func>
+            void traversePostOrder(Func function, BSTNode* node) const;
+
+            unsigned int getHeight(BSTNode* node) const;
+            unsigned int getNumberOfLeafs(BSTNode* node) const;
+            bool isBalanced(BSTNode* node) const;
     };
 
 }
